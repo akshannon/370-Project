1d4abdf8ac12383a84d33acd469aacce607f80f2
akshannon <alijah.shannon@colostate.edu>
2026-04-28 16:25:44 -0600

    fix error in engine

--- Engine.py
+++ Engine.py
@@ -5,5 +5,5 @@ import camera
 def main():
     rgb = camera.takePicture() # camera take a picture call function
     first_color = ColorDict.get_closest_color(rgb)
-    coor_color = ColorDict.get_complement_color(rgb)
+    coor_color = ColorDict.get_complement_color(first_color)
     # send color w socket
