5aed18287dbd8d83519a6e090eb2b7aa25b4a5dc
MakaelaRM <makaela.maryanski@gmail.com>
2026-04-30 14:34:44 -0600

    m

--- Desktop.py
+++ Desktop.py
@@ -4,15 +4,15 @@ import json
 
 PORT = 65432
 
-engine = pyttsx3.init()
 # Tweak voice/rate/volume here if needed
 # engine.setProperty('rate', 150)
 # engine.setProperty('volume', 1.0)
 
 def text_to_speech(current_color, complement_color):
-    engine.stop()
+    engine = pyttsx3.init()
     engine.say(f'This item is {current_color}. You can pair it with {complement_color}.')
     engine.runAndWait()
+    engine.stop()
 
 def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
